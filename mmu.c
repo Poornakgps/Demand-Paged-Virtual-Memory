1ea7d3e12127bc0a8b422608a7c8a244d6d90a73
Poornachandra123 <96694008+Poornachandra123@users.noreply.github.com>
2025-05-26 21:56:34 +0530

    updated implementations

--- mmu.c
+++ mmu.c
@@ -16,102 +16,6 @@
 #include <sys/stat.h>
 #include <sys/wait.h>
 
-<<<<<<< HEAD
-
-#define PSEND_TYPE 10
-#define MMUTOPRO 20
-#define INVALID_PAGE_REF -2
-#define PAGEFAULT -1
-#define PROCESS_OVER -9
-#define PAGEFAULT_HANDLED 5
-#define TERMINATED 10
-
-int count = 0;
-int *pffreq;
-FILE *resultf;
-int i;
-
-typedef struct {
-	int frameno;
-	int isvalid;
-	int count;
-} ptbentry;
-
-typedef struct {
-	pid_t pid;
-	int m;
-	int f_cnt;
-	int f_allo;
-} pcb;
-
-typedef struct
-{
-	int current;
-	int flist[];
-} freelist;
-
-struct msgbuf
-{
-	long mtype;
-	int id;
-	int pageno;
-};
-
-struct mmutopbuf
-{
-	long mtype;
-	int frameno;
-};
-
-struct mmutosch
-{
-	long mtype;
-	char mbuf[1];
-};
-
-
-key_t freekey, pagetbkey;
-key_t msgq2key, msgq3key;
-key_t pcbkey;
-
-int ptbid, freelid;
-int msgq2id, msgq3id;
-int pcbid;
-
-
-int m,k;
-
-int readRequest(int* id)
-{
-	struct msgbuf mbuf;
-	int length;
-	/* The length is essentially the size of the structure minus sizeof(mtype) */
-	length = sizeof(struct msgbuf) - sizeof(long);
-	memset(&mbuf, 0, sizeof(mbuf));
-
-	int rst = msgrcv(msgq3id, &mbuf, length, PSEND_TYPE, 0);
-	if (rst == -1)
-	{
-		if(errno == EINTR)
-			return -1;
-		perror("msgrcv");
-		exit(EXIT_FAILURE);
-	}
-	*id = mbuf.id;
-	return mbuf.pageno;
-}
-
-void sendreply(int id, int frameno)
-{
-	struct mmutopbuf mbuf;
-	mbuf.mtype = id + MMUTOPRO;
-	mbuf.frameno = frameno;
-	int length = sizeof(struct msgbuf) - sizeof(long);
-	int rst = msgsnd(msgq3id, &mbuf, length, 0);
-	if (rst == -1)
-	{
-		perror("msgsnd");
-=======
 #define PROCESS_REQUEST_TYPE 10
 #define MMU_TO_PROCESS_TYPE 20
 #define INVALID_PAGE_REFERENCE -2
@@ -202,169 +106,10 @@ void send_response_to_process(int process_id, int frame_number)
 	if (send_result == -1)
 	{
 		perror("message_send");
->>>>>>> 92241c1 (Updated implementations)
 		exit(EXIT_FAILURE);
 	}
 }
 
-<<<<<<< HEAD
-void notifySched(int type)
-{
-	struct mmutosch mbuf;
-	mbuf.mtype = type;
-	int length = sizeof(struct msgbuf) - sizeof(long);
-	int rst = msgsnd(msgq2id, &mbuf, length, 0);
-	if (rst == -1)
-	{
-		perror("msgsnd");
-		exit(EXIT_FAILURE);
-	}
-	//printf("Sent signal to sched = %d\n",type);
-
-}
-pcb *pcbptr;
-ptbentry *ptbptr;
-freelist *freeptr;
-
-int handlePageFault(int id, int pageno)
-{
-	int i;
-	if (freeptr->current == -1 || pcbptr[i].f_cnt <= pcbptr[i].f_allo)
-	{
-		int min = INT_MAX, mini = -1;
-		int victim = 0;
-		for (i = 0; i < pcbptr[i].m; i++)
-		{
-			if (ptbptr[id * m + i].isvalid == 1)
-			{
-				if (ptbptr[id * m + i].count < min)
-				{
-					min = ptbptr[id * m + i].count;
-					victim = ptbptr[id * m + i].frameno;
-					mini = i;
-				}
-			}
-		}
-		ptbptr[id * m + mini].isvalid = 0;
-		return victim;
-	}
-	else
-	{
-		int fn = freeptr->flist[freeptr->current];
-		freeptr->current -= 1;
-		return fn;
-	}
-}
-
-void freepages(int i)
-{
-
-	int k = 0;
-	for (k = 0; k < pcbptr[i].m; i++)
-	{
-		if (ptbptr[i * m + k].isvalid == 1)
-		{
-			freeptr->flist[freeptr->current + 1] = ptbptr[i * m + k].frameno;
-			freeptr->current += 1;
-		}
-	}
-	//TODO: Think shuld change the allocation
-}
-
-int serviceMRequest()
-{
-	pcbptr = (pcb*)(shmat(pcbid, NULL, 0));
-	/*if (*(int *)pcbptr == -1)
-	{
-		perror("pcb-shmat");
-		exit(EXIT_FAILURE);
-	}*/
-	ptbptr = (ptbentry*)(shmat(ptbid, NULL, 0));
-	/*if (*(int *)ptbptr == -1)
-	{
-		perror("pcb-shmat");
-		exit(EXIT_FAILURE);
-	}*/
-	freeptr = (freelist*)(shmat(freelid, NULL, 0));
-	/*if (*((int *)freeptr) == -1)
-	{
-		perror("freel-shmat");
-		exit(EXIT_FAILURE);
-	}*/
-
-	int id = -1, pageno;
-	pageno = readRequest(&id);
-	if(pageno == -1 && id == -1)
-	{
-		return 0;
-	}
-	int i = id;
-	if (pageno == PROCESS_OVER)
-	{
-		freepages(id);
-		notifySched(TERMINATED);
-		//printf("Notifying notifySched\n");
-		return;
-	}
-	count ++;
-	printf("Page reference : (%d,%d,%d)\n",count,id,pageno);
-	fprintf(resultf,"Page reference : (%d,%d,%d)\n",count,id,pageno);
-	if (pcbptr[id].m < pageno || pageno < 0)
-	{
-		printf("Invalid Page Reference : (%d %d)\n",id,pageno);
-		fprintf(resultf,"Invalid Page Reference : (%d %d)\n",id,pageno);
-		sendreply(id, INVALID_PAGE_REF);
-		printf("Process %d: TRYING TO ACCESS INVALID PAGE REFERENCE %d\n", id, pageno);
-		freepages(id);
-		notifySched(TERMINATED);
-		//Invalid reference
-
-	}
-	else
-	{
-		if (ptbptr[i * m + pageno].isvalid == 0)
-		{
-			//PAGE FAULT
-			printf("Page Fault : (%d, %d)\n",id,pageno);
-			fprintf(resultf,"Page Fault : (%d, %d)\n",id,pageno);
-			pffreq[id] += 1;
-			sendreply(id, -1);
-			int fno = handlePageFault(id, pageno);
-			ptbptr[i * m + pageno].isvalid = 1;
-			ptbptr[i * m + pageno].count = count;
-			ptbptr[i * m + pageno].frameno = fno;
-			
-			notifySched(PAGEFAULT_HANDLED);
-		}
-		else
-		{
-			sendreply(id, ptbptr[i * m + pageno].frameno);
-			ptbptr[i * m + pageno].count = count;
-			//FRAME FOUND
-		}
-	}
-	if(shmdt(pcbptr) == -1)
-	{
-		perror("pcbptr-shmdt");
-		exit(EXIT_FAILURE);
-	}
-	if(shmdt(ptbptr) == -1)
-	{
-		perror("ptbptr-shmdt");
-		exit(EXIT_FAILURE);
-	}
-	if(shmdt(freeptr) == -1)
-	{
-		perror("freel-shmdt");
-		exit(EXIT_FAILURE);
-	}
-}
-int flag = 1;
-void handletgerm(int sig)
-{
-	//printf("I am a god kill me\n");
-	flag = 0;
-=======
 void notify_scheduler(int notification_type)
 {
 	struct mmu_scheduler_notification notification_message;
@@ -500,46 +245,10 @@ int continue_execution = 1;
 void handle_termination_signal(int signal_number)
 {
 	continue_execution = 0;
->>>>>>> 92241c1 (Updated implementations)
 }
 
 int main(int argc, char const *argv[])
 {
-<<<<<<< HEAD
-	if (argc < 4)
-	{
-		printf("mmu m2key m3key ptbkey fkey pcbkey m k\n");
-		exit(EXIT_FAILURE);
-	}
-	msgq2id = atoi(argv[1]);
-	msgq3id = atoi(argv[2]);
-	ptbid = atoi(argv[3]);
-	freelid = atoi(argv[4]);
-	pcbid = atoi(argv[5]);
-	m = atoi(argv[6]);
-	k = atoi(argv[7]);
-	signal(SIGUSR2, handletgerm);
-	pffreq = (int *)malloc(k*sizeof(int));
-	for(i=0;i<k;i++)
-	{
-		pffreq[i] = 0;
-	} 
-	resultf = fopen("result.txt","w");
-	while(flag)
-	{
-		serviceMRequest();
-	}
-	printf("Page fault Count for each Process:\n");	
-	fprintf(resultf,"Page fault Count for each Process:\n");
-	printf("Process Id\tFreq\n");
-	fprintf(resultf,"Process Id\tFreq\n");
-	for(i = 0;i<k;i++)
-	{
-		printf("%d\t%d\n",i,pffreq[i]);
-		fprintf(resultf,"%d\t%d\n",i,pffreq[i]);
-	}
-	fclose(resultf);
-=======
 	if (argc < 8)
 	{
 		printf("mmu scheduler_mmu_queue_id process_mmu_queue_id page_table_shared_id frame_list_shared_id pcb_shared_memory_id pages_per_process total_processes\n");
@@ -573,6 +282,4 @@ int main(int argc, char const *argv[])
 		fprintf(result_file, "%d\t%d\n", loop_index, page_fault_frequency[loop_index]);
 	}
 	fclose(result_file);
->>>>>>> 92241c1 (Updated implementations)
-	return 0;
-}
+implementations)
--- sched2.c
+++ sched2.c
@@ -18,43 +18,6 @@
 #include <ctype.h>
 #include <sys/shm.h>
 
-<<<<<<< HEAD
-
-#define MAX_BUFFER_SIZE 100
-#define MAX_PAGES 1000
-#define MAX_PROCESS 1000
-#define FROMPROCESS 10
-#define TOPROCESS 20  // TOPROCESS+id will be used to read msg from sch
-// #define TOMMU 10
-#define FROMMMU 20
-
-#define PAGEFAULT_HANDLED 5
-#define TERMINATED 10
-
-int k; //no. of processes
-typedef struct _mmutosch {
-	long    mtype;
-	char mbuf[1];
-} mmutosch;
-
-// int running[MAX_PROCESS];
-// int runh,runt;
-
-typedef struct mymsgbuf {
-	long    mtype;
-	int id;
-} mymsgbuf;
-
-
-int send_message( int qid, struct mymsgbuf *qbuf )
-{
-	int     result, length;
-
-	/* The length is essentially the size of the structure minus sizeof(mtype) */
-	length = sizeof(struct mymsgbuf) - sizeof(long);
-
-	if ((result = msgsnd( qid, qbuf, length, 0)) == -1)
-=======
 #define MAX_BUFFER_SIZE 100
 #define MAX_PAGES 1000
 #define MAX_PROCESS 1000
@@ -84,25 +47,11 @@ int send_message_to_process(int queue_id, struct scheduler_process_message *mess
 	message_length = sizeof(struct scheduler_process_message) - sizeof(long);
 
 	if ((send_result = msgsnd(queue_id, message_buffer, message_length, 0)) == -1)
->>>>>>> 92241c1 (Updated implementations)
 	{
 		perror("Error in sending message");
 		exit(1);
 	}
 
-<<<<<<< HEAD
-	return (result);
-}
-
-int read_message( int qid, long type, struct mymsgbuf *qbuf )
-{
-	int     result, length;
-
-	/* The length is essentially the size of the structure minus sizeof(mtype) */
-	length = sizeof(struct mymsgbuf) - sizeof(long);
-
-	if ((result = msgrcv( qid, qbuf, length, type,  0)) == -1)
-=======
 	return (send_result);
 }
 
@@ -113,25 +62,11 @@ int receive_message_from_process(int queue_id, long message_type, struct schedul
 	message_length = sizeof(struct scheduler_process_message) - sizeof(long);
 
 	if ((receive_result = msgrcv(queue_id, message_buffer, message_length, message_type, 0)) == -1)
->>>>>>> 92241c1 (Updated implementations)
 	{
 		perror("Error in receiving message");
 		exit(1);
 	}
 
-<<<<<<< HEAD
-	return (result);
-}
-
-int read_message_mmu( int qid, long type,mmutosch *qbuf )
-{
-	int result, length;
-
-	/* The length is essentially the size of the structure minus sizeof(mtype) */
-	length = sizeof(mmutosch) - sizeof(long);
-
-	if ((result = msgrcv(qid, qbuf, length, type,  0)) == -1)
-=======
 	return (receive_result);
 }
 
@@ -142,69 +77,11 @@ int receive_message_from_mmu(int queue_id, long message_type, mmu_to_scheduler_m
 	message_length = sizeof(mmu_to_scheduler_message) - sizeof(long);
 
 	if ((receive_result = msgrcv(queue_id, message_buffer, message_length, message_type, 0)) == -1)
->>>>>>> 92241c1 (Updated implementations)
 	{
 		perror("Error in receiving message");
 		exit(1);
 	}
 
-<<<<<<< HEAD
-	return (result);
-}
-int main(int argc , char * argv[])
-{
-	int mq1_key, mq2_key, master_pid;
-	if (argc < 5) {
-		printf("Scheduler rkey q2key k mpid\n");
-		exit(EXIT_FAILURE);
-	}
-	mq1_key = atoi(argv[1]);
-	mq2_key = atoi(argv[2]);
-	k = atoi(argv[3]);
-	master_pid = atoi(argv[4]);
-
-	mymsgbuf msg_send, msg_recv;
-
-	int mq1 = msgget(mq1_key, 0666);
-	int mq2 = msgget(mq2_key, 0666);
-	if (mq1 == -1)
-	{
-		perror("Message Queue1 creation failed");
-		exit(1);
-	}
-	if (mq2 == -1)
-	{
-		perror("Message Queue2 creation failed");
-		exit(1);
-	}
-	printf("Total No. of Process received = %d\n", k);
-
-	//initialize the variables for running array
-	int terminated_process = 0; //to keep track of running process to exit at last
-	// int terminate[MAX_PROCESS];
-	while (1)
-	{
-		read_message(mq1, FROMPROCESS, &msg_recv);
-		int curr_id = msg_recv.id;
-
-		msg_send.mtype = TOPROCESS + curr_id;
-		send_message(mq1, &msg_send);
-
-		//recv messages from mmu
-		mmutosch mmu_recv;
-		read_message_mmu(mq2, 0, &mmu_recv);
-		//printf("received %ld\n", mmu_recv.mtype);
-		if (mmu_recv.mtype == PAGEFAULT_HANDLED)
-		{
-			msg_send.mtype = FROMPROCESS;
-			msg_send.id=curr_id;
-			send_message(mq1, &msg_send);
-		}
-		else if (mmu_recv.mtype == TERMINATED)
-		{
-			terminated_process++;
-			//printf("Got terminate %d\n", curr_id);
-=======
 	return (receive_result);
 }
 
@@ -256,24 +133,12 @@ int main(int argc, char * argv[])
 		else if (mmu_notification.message_type == PROCESS_TERMINATED)
 		{
 			completed_processes_count++;
->>>>>>> 92241c1 (Updated implementations)
 		}
 		else
 		{
 			perror("Wrong message from mmu\n");
 			exit(1);
 		}
-<<<<<<< HEAD
-		if (terminated_process == k)
-			break;
-		//printf("====Term %d====\n", terminated_process);
-	}
-	//printf("Sending sinal\n");
-	kill(master_pid, SIGUSR1);
-	pause();
-	printf("Scheduler terminating ...\n") ;
-	exit(1) ;
-=======
 		if (completed_processes_count == total_processes)
 			break;
 	}
@@ -281,5 +146,4 @@ int main(int argc, char * argv[])
 	pause();
 	printf("Scheduler terminating ...\n");
 	exit(1);
->>>>>>> 92241c1 (Updated implementations)
 }
